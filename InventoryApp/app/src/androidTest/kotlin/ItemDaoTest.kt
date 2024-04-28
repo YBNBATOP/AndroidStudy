caec21cca98707a29f62bc9f7e86379beca6cdac
YBNBATOP <ciserafim2003@gmail.com>
2024-04-28 13:01:25 +0200

    feat: adding small test to get an item from db

--- InventoryApp/app/src/androidTest/kotlin/ItemDaoTest.kt
+++ InventoryApp/app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -80,6 +80,15 @@ class ItemDaoTest {
         assertTrue(allItems.isEmpty())
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoGetItem_returnsItemFromDB() = runBlocking {
+        addOneItemToDb()
+        val item = itemDao.getItem(1)
+        assertEquals(item.first(), item1)
+    }
+
+
     @After
     @Throws(IOException::class)
     fun closeDb() {
